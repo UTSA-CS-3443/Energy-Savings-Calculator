ef74f1ad04a822ca45fd39e33514ddbb49c9924b
Maximilian <maximilianguzman@gmail.com>
2017-11-10 23:58:33 -0600

    Updated GUI and simple save file system created.

--- src/main/java/com/unknown8/esc/controller/MainViewController.java
+++ src/main/java/com/unknown8/esc/controller/MainViewController.java
@@ -1,10 +1,16 @@
 package com.unknown8.esc.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 
 public class MainViewController implements EventHandler<ActionEvent> {
@@ -21,7 +27,7 @@ public class MainViewController implements EventHandler<ActionEvent> {
 	@FXML
 	private TextField numWindows;
 	@FXML
-	private	ComboBox<String> windowType;
+	private ComboBox<String> windowType;
 	@FXML
 	private TextField numDoors;
 	@FXML
@@ -48,78 +54,168 @@ public class MainViewController implements EventHandler<ActionEvent> {
 
 	@FXML
 	Button confirm;
-	
+	@FXML
+	Button clear;
+	@FXML
+	private MenuItem Save;
+
 	private String typeWindow, typeDoor;
 	private double woAC, wAC, inTemp, outTemp, wallLength;
 	private int numWin, numDoor;
-	
+
 	@FXML
 	public void initialize() {
-	    windowType.getItems().clear();
-	    windowType.getItems().addAll("Single pane", "Double Pane", "Triple Pane");
-	    windowType.getSelectionModel().select("Type");
-	    
-	    doorType.getItems().clear();
-	    doorType.getItems().addAll("Particle board", "Hardwood");
-	    doorType.getSelectionModel().select("Type");
+		windowType.getItems().clear();
+		windowType.getItems().addAll("Single pane", "Double Pane", "Triple Pane");
+		windowType.getSelectionModel().select("Type");
+
+		doorType.getItems().clear();
+		doorType.getItems().addAll("Particle board", "Hardwood");
+		doorType.getSelectionModel().select("Type");
 	}
 
 	@Override
 	public void handle(ActionEvent e) {
-	
+
 		try {
-		woAC = Double.parseDouble(woACBill.getText());
-		wAC = Double.parseDouble(wACBill.getText());
-		inTemp = Double.parseDouble(targetTempC.getText());
-		outTemp = Double.parseDouble(outdoorTempC.getText());
-		wallLength = Double.parseDouble(extWallLength.getText());
-		numWin = Integer.parseInt(numWindows.getText());
-		numDoor = Integer.parseInt(numDoors.getText());
-		typeWindow = windowType.getValue();
-		typeDoor = doorType.getValue();
-		if (typeWindow == "Type" || typeDoor == "Type") {
+			woAC = Double.parseDouble(woACBill.getText());
+			wAC = Double.parseDouble(wACBill.getText());
+			inTemp = Double.parseDouble(targetTempC.getText());
+			outTemp = Double.parseDouble(outdoorTempC.getText());
+			wallLength = Double.parseDouble(extWallLength.getText());
+			numWin = Integer.parseInt(numWindows.getText());
+			numDoor = Integer.parseInt(numDoors.getText());
+			typeWindow = windowType.getValue();
+			typeDoor = doorType.getValue();
+			if (typeWindow == "Type" || typeDoor == "Type") {
+				System.out.println("Fill in every box.");
+
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Fill In Everything");
+				alert.setHeaderText(null);
+				alert.setContentText("Fill in every box and make selections!");
+				alert.showAndWait();
+				return;
+			}
+		} catch (Exception e1) {
 			System.out.println("Fill in every box.");
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Fill In Everything");
+			alert.setHeaderText(null);
+			alert.setContentText("Fill in every box and make selections!");
+			alert.showAndWait();
 			return;
 		}
-		}catch(Exception e1) {
-			System.out.println("Fill in every box.");
-			return;
-		}
-		out1.setEditable(true);		
+		out1.setEditable(true);
 		out1.setText(String.valueOf(woAC));
-		out1.setEditable(false);	
-		
-		out2.setEditable(true);		
+		out1.setEditable(false);
+
+		out2.setEditable(true);
 		out2.setText(String.valueOf(wAC));
-		out2.setEditable(false);	
-		
-		out3.setEditable(true);		
+		out2.setEditable(false);
+
+		out3.setEditable(true);
 		out3.setText(String.valueOf(inTemp));
-		out3.setEditable(false);	
-		
-		out4.setEditable(true);		
+		out3.setEditable(false);
+
+		out4.setEditable(true);
 		out4.setText(String.valueOf(outTemp));
 		out4.setEditable(false);
-		
-		out5.setEditable(true);		
+
+		out5.setEditable(true);
 		out5.setText(String.valueOf(wallLength));
-		out5.setEditable(false);	
-		
-		out6.setEditable(true);		
+		out5.setEditable(false);
+
+		out6.setEditable(true);
 		out6.setText(String.valueOf(numWin));
-		out6.setEditable(false);	
-		
-		out7.setEditable(true);		
+		out6.setEditable(false);
+
+		out7.setEditable(true);
 		out7.setText(String.valueOf(numDoor));
-		out7.setEditable(false);	
-		
-		out8.setEditable(true);		
+		out7.setEditable(false);
+
+		out8.setEditable(true);
 		out8.setText(typeWindow);
-		out8.setEditable(false);	
-		
-		out9.setEditable(true);		
+		out8.setEditable(false);
+
+		out9.setEditable(true);
 		out9.setText(typeDoor);
-		out9.setEditable(false);	
+		out9.setEditable(false);
 
 	}
+
+	@FXML
+	public void clearFields(ActionEvent e) {
+		// Clear the comboBox via initialize
+		initialize();
+		// Clear stored values
+		woAC = wAC = inTemp = outTemp = wallLength = numWin = numDoor = 0;
+
+		numWindows.setText("");
+		numDoors.setText("");
+		extWallLength.setText("");
+		targetTempC.setText("");
+		outdoorTempC.setText("");
+		woACBill.setText("");
+		wACBill.setText("");
+
+		// Clear the output
+		out1.setEditable(true);
+		out1.setText("");
+		out1.setEditable(false);
+
+		out2.setEditable(true);
+		out2.setText("");
+		out2.setEditable(false);
+
+		out3.setEditable(true);
+		out3.setText("");
+		out3.setEditable(false);
+
+		out4.setEditable(true);
+		out4.setText("");
+		out4.setEditable(false);
+
+		out5.setEditable(true);
+		out5.setText("");
+		out5.setEditable(false);
+
+		out6.setEditable(true);
+		out6.setText("");
+		out6.setEditable(false);
+
+		out7.setEditable(true);
+		out7.setText("");
+		out7.setEditable(false);
+
+		out8.setEditable(true);
+		out8.setText("");
+		out8.setEditable(false);
+
+		out9.setEditable(true);
+		out9.setText("");
+		out9.setEditable(false);
+
+	}
+
+	@FXML
+	private void save(ActionEvent e) {
+		try {
+			File sf = new File("ESCsaveFile.txt");
+			PrintStream saveFile = new PrintStream(sf);
+			saveFile.println(typeWindow);
+			saveFile.println(typeDoor);
+			saveFile.println(woAC);
+			saveFile.println(wAC);
+			saveFile.println(inTemp);
+			saveFile.println(outTemp);
+			saveFile.println(wallLength);
+			saveFile.println(numWin);
+			saveFile.println(numDoor);
+			saveFile.close();
+		} catch (FileNotFoundException ee) {
+			System.out.println("ESCsaveFile.txt was moved");
+		}
+	}
+
 }
